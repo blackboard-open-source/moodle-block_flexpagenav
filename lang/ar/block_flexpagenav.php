06ff212a684a7d470a0d8c3c2ad1f8d5d89286db
vagrant <vagrant@vagrant.vm>
2018-01-24 16:45:10 +0000

    INT-12838: Importing ar lang

---
+++ lang/ar/block_flexpagenav.php
@@ -0,0 +1,108 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'قائمة Flexpage';
+$string['flexpagenav:view'] = 'عرض قوائم Flexpage';
+$string['flexpagenav:manage'] = 'إدارة قوائم Flexpage';
+$string['flexpagenav:addinstance'] = 'إضافة كتلة قائمة Flexpage جديدة';
+$string['addexistingmenuaction'] = 'إضافة قائمة موجودة';
+$string['managemenusaction'] = 'إدارة جميع القوائم';
+$string['managemenus'] = 'إدارة القوائم';
+$string['name'] = 'الاسم';
+$string['manage'] = 'إدارة';
+$string['usedastabs'] = 'مستخدم كعلامات تبويب';
+$string['addmenudotdotdot'] = 'إضافة قائمة جديدة...';
+$string['editmenu'] = 'القائمة تحرير';
+$string['useastab'] = 'استخدام كعلامات تبويب';
+$string['displayname'] = 'اسم العرض';
+$string['render'] = 'عرض قائمة كـ';
+$string['tree'] = 'شجرة';
+$string['dropdown'] = 'قائمة منسدلة';
+$string['navhorizontal'] = 'التنقل الأفقي';
+$string['navvertical'] = 'التنقل العمودي';
+$string['ajaxexception'] = 'خطأ في التطبيق: {$a}';
+$string['editmenuaction'] = 'القائمة تحرير';
+$string['deletemenuaction'] = 'حذف';
+$string['managelinksaction'] = 'إدارة الروابط';
+$string['managelinks'] = 'إدارة الروابط';
+$string['editlink'] = 'تحرير';
+$string['movelink'] = 'نقل رابط';
+$string['deletelink'] = 'حذف';
+$string['type'] = 'النوع';
+$string['preview'] = 'معاينة/معلومات';
+$string['urllink'] = 'عنوان URL';
+$string['addlinkdotdotdot'] = 'إضافة رابط جديد...';
+$string['editingx'] = 'تحرير {$a}';
+$string['label'] = 'تسمية';
+$string['url'] = 'عنوان URL';
+$string['labelurlrequired'] = 'حقلا التسمية وعنوان URL مطلوبين';
+$string['editlinkaction'] = 'تحرير';
+$string['movelinkaction'] = 'نقل';
+$string['deletelinkaction'] = 'حذف';
+$string['movelinkx'] = 'نقل رابط {$a}';
+$string['movebefore'] = 'قبل';
+$string['moveafter'] = 'بعد';
+$string['areyousuredeletelink'] = '<p>هل تريد بالتأكيد حذف هذا الرابط؟</p><p>{$a}</p>';
+$string['areyousuredeletemenu'] = 'هل تريد بالتأكيد حذف القائمة <strong>{$a}</strong>؟';
+$string['deletemenu'] = 'حذف قائمة';
+$string['displaymenu'] = 'عرض قائمة';
+$string['flexpage'] = 'Flexpage';
+$string['flexpagelink'] = 'Flexpage';
+$string['includechildren'] = 'تضمين العناصر الفرعية';
+$string['excludechildren'] = 'العناصر الفرعية المراد تضمينها';
+$string['nochildpages'] = 'لا توجد عناصر فرعية لـ flexpages';
+$string['flexpagewithchildren'] = '{$a} مع عناصر فرعية';
+$string['flexpagewithoutchildren'] = '{$a} بدون عناصر فرعية';
+$string['flexpageerror'] = 'خطأ: على الأرجح Flexpage لم يعد موجودًا';
+$string['flexpagenav'] = 'قائمة Flexpage';
+$string['flexpagenavlink'] = 'قائمة Flexpage';
+$string['flexpagenavx'] = '{$a} قائمة';
+$string['flexpagenaverror'] = 'خطأ: على الأرجح قائمة Flexpage لم تعد موجودة';
+$string['nomenustoadd'] = 'لا توجد قوائم Flexpage أخرى يمكنك إضافتها إلى هذه القائمة.';
+$string['coursemodule'] = 'نشاط المقرر الدراسي';
+$string['modlink'] = 'النشاط';
+$string['ticketlink'] = 'بطاقة إفادة بالمشكلة';
+$string['subject'] = 'الموضوع';
+$string['labelrequired'] = 'حقل التسمية مطلوب.';
+$string['menus'] = 'القوائم';
+$string['addexistingmenu'] = 'إضافة قائمة موجودة';
+$string['addexistingmenu_help'] = 'اختر المكان الذي تريد وضع الكتلة فيه على flexpage عن طريق تحديد أحد الأزرار الموجودة في أعلى نافذة <strong>إضافة كتلة موجودة</strong>. ثم، انقر فوق اسم القائمة التي ترغب في إضافتها إلى المقرر الدراسي.';
+$string['migrationtoptabs'] = 'أهم علامات التبويب للمقرر الدراسي';
+$string['moderror'] = 'خطأ: على الأرجح النشاط لم يعد موجودًا';
+$string['noexistingmenustoadd'] = 'لم يتم إنشاء قوائم لهذا المقرر الدراسي.  استخدم <strong>إدارة > إدارة جميع القوائم</strong> لإنشاء قوائم جديدة.';
+$string['urlfailedcleaning'] = 'فشل عنوان URL الذي تم إدخاله في التحقق من صحة العملية وإزالتها.  يرجى التأكد من أن عنوان URL الذي تم إدخاله صالح.';
+$string['urlmuststartwith'] = 'يجب أن يبدأ عنوان URL الذي تم إدخاله إما بـ http://‎ أو https://‎';
+$string['managinglinksforx'] = 'إدارة روابط لقائمة <em>{$a}</em>';
+$string['formnamerequired'] = 'اسم القائمة مطلوب.';
+$string['invalidurl'] = 'عنوان URL الذي تم إدخاله غير صالح ولا يمكن استخدامه.  يرجى التحقق من صحة عنوان URL الذي تم بإدخاله.';
+$string['menudisplayerror'] = 'تعذر عرض قائمة Flexpage المرتبطة.  قائمة Flexpage إما أن تكون مقطوعة أو لم تعد موجودة.  يرجى إما حذف هذه الكتلة أو تحريرها لعرض قائمة Flexpage مختلفة.';
+$string['dockable'] = 'السماح بإرساء هذه الكتلة';
+$string['dockable_help'] = 'حدد ما إذا كان من الممكن إرساء هذه الكتلة بواسطة المستخدم أم لا.  يرجى ملاحظة إمكانية تعطيل الإرساء بواسطة الاستثناءات التالية:
+<ul>
+<li>يمكن أن يمنع إعداد الشكل إرساء الكتل.</li>
+<li>لا يمكن إرساء إلا قوائم Flexpage التي يتم عرضها بتنسيق الشجر فقط.</li>
+<li>لا يمكن إرساء الكتل التي لا يوجد لها عناوين.</li>
+</ul>';
+$string['managemenus_help'] = '<p>توفر القوائم التنقل اللازم للمستخدمين للتنقل خلال flexpages الخاص بالمقرر الدراسي. يمكن وضع القوائم في أي مناطق لـ flexpage ككتل. يمكن أن تحتوي القوائم على روابط إلى flexpages داخل المقررات الدراسية، وعناوين URL الخارجية، وروابط لقوائم أخرى.</p>
+
+<p>يمكن كذلك وضع علامة على أحد القوائم الموجودة في مقرر دراسي باستخدامها "علامات تبويب". ستظهر هذه القائمة كـ "أهم علامات التبويب" في المقرر الدراسي. إذا كنت ترغب في أن تقوم كل علامة تبويب بتقديم خيارات متعددة، قم بإنشاء القائمة "أهم علامة تبويب" والارتباط بقائمة Flexpage مختلفة لكل علامة تبويب.</p>';
