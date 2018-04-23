98471dd7c59d4213e4d9a68f1cdd5c129dbf29ea
David Castro <david.castro@blackboard.com>
2018-04-23 15:33:11 +0000

    INT-13192: Importing de lang

---
+++ lang/de/block_flexpagenav.php
@@ -0,0 +1,107 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Flexpage-Menü';
+$string['flexpagenav:view'] = 'Flexpage-Menüs anzeigen';
+$string['flexpagenav:manage'] = 'Flexpage-Menüs verwalten';
+$string['flexpagenav:addinstance'] = 'Neuen Block "Flexpage-Menü" hinzufügen';
+$string['addexistingmenuaction'] = 'Vorhandenes Menü hinzufügen';
+$string['managemenusaction'] = 'Alle Menüs verwalten';
+$string['managemenus'] = 'Menüs verwalten';
+$string['name'] = 'Name';
+$string['manage'] = 'Verwalten';
+$string['usedastabs'] = 'Als Registerkarten verwendet';
+$string['addmenudotdotdot'] = 'Neues Menü hinzufügen...';
+$string['editmenu'] = 'Menü bearbeiten';
+$string['useastab'] = 'Als Registerkarten verwenden';
+$string['displayname'] = 'Name anzeigen';
+$string['render'] = 'Menü anzeigen als';
+$string['tree'] = 'Struktur';
+$string['dropdown'] = 'Dropdown';
+$string['navhorizontal'] = 'Horizontale Navigation';
+$string['navvertical'] = 'Vertikale Navigation';
+$string['ajaxexception'] = 'Anwendungsfehler: {$a}';
+$string['editmenuaction'] = 'Menü bearbeiten';
+$string['deletemenuaction'] = 'Löschen';
+$string['managelinksaction'] = 'Links verwalten';
+$string['managelinks'] = 'Links verwalten';
+$string['editlink'] = 'Bearbeiten';
+$string['movelink'] = 'Link verschieben';
+$string['deletelink'] = 'Löschen';
+$string['type'] = 'Typ';
+$string['preview'] = 'Vorschau/Info';
+$string['urllink'] = 'URL';
+$string['addlinkdotdotdot'] = 'Neuen Link hinzufügen...';
+$string['editingx'] = '{$a} wird bearbeitet';
+$string['label'] = 'Label';
+$string['url'] = 'URL';
+$string['labelurlrequired'] = 'Die Felder "Label" und "URL" sind erforderlich.';
+$string['editlinkaction'] = 'Bearbeiten';
+$string['movelinkaction'] = 'Verschieben';
+$string['deletelinkaction'] = 'Löschen';
+$string['movelinkx'] = 'Link {$a} verschieben';
+$string['movebefore'] = 'vor';
+$string['moveafter'] = 'nach';
+$string['areyousuredeletelink'] = '<p>Möchten Sie diesen Link wirklich löschen?</p><p>{$a}</p>';
+$string['areyousuredeletemenu'] = 'Sind Sie sicher, dass Sie das Menü <strong>{$a}</strong> löschen möchten?';
+$string['deletemenu'] = 'Menü löschen';
+$string['displaymenu'] = 'Menü anzeigen';
+$string['flexpage'] = 'Flexpage';
+$string['flexpagelink'] = 'Flexpage';
+$string['includechildren'] = 'Untergeordnete Elemente einbeziehen';
+$string['excludechildren'] = 'Einzubeziehende untergeordnete Elemente';
+$string['nochildpages'] = 'Keine untergeordneten Flexpages';
+$string['flexpagewithchildren'] = '{$a} mit untergeordneten Elementen';
+$string['flexpagewithoutchildren'] = '{$a} ohne untergeordnete Elemente';
+$string['flexpageerror'] = 'Fehler: Flexpage vermutlich nicht mehr vorhanden';
+$string['flexpagenav'] = 'Flexpage-Menü';
+$string['flexpagenavlink'] = 'Flexpage-Menü';
+$string['flexpagenavx'] = '{$a}-Menü';
+$string['flexpagenaverror'] = 'Fehler: Flexpage-Menü vermutlich nicht mehr vorhanden';
+$string['nomenustoadd'] = 'Es gibt keine weiteren Flexpage-Menüs, die Sie diesem Menü hinzufügen können.';
+$string['coursemodule'] = 'Kursaktivität';
+$string['modlink'] = 'Aktivität';
+$string['ticketlink'] = 'Trouble Ticket';
+$string['subject'] = 'Thema';
+$string['labelrequired'] = 'Im Feld "Label" ist eine Eingabe erforderlich.';
+$string['menus'] = 'Menüs';
+$string['addexistingmenu'] = 'Vorhandenes Menü hinzufügen';
+$string['addexistingmenu_help'] = 'Indem Sie eine der Schaltflächen oben im Fenster </strong>Vorhandenes Menü hinzufügen<strong> auswählen, können Sie festlegen, wo der Block auf der Flexpage platziert werden soll. Klicken Sie anschließend auf den Namen des Menüs, das Sie dem Kurs hinzufügen möchten.';
+$string['migrationtoptabs'] = 'Hauptregisterkarten des Kurses';
+$string['moderror'] = 'Fehler: Aktivität vermutlich nicht mehr vorhanden';
+$string['noexistingmenustoadd'] = 'Für diesen Kurs wurden keine Menüs erstellt. Verwenden Sie <strong>Verwalten > Alle Menüs verwalten</strong>, um neue Menüs zu erstellen.';
+$string['urlfailedcleaning'] = 'Der Prüfungs- und Bereinigungsprozess für die eingegebene URL konnte nicht erfolgreich abgeschlossen werden. Stellen Sie sicher, dass die eingegebene URL gültig ist.';
+$string['urlmuststartwith'] = 'Die eingegebene URL muss mit http:// oder https:// beginnen.';
+$string['managinglinksforx'] = 'Verwalten von Links für Menü <em>{$a}</em>';
+$string['formnamerequired'] = 'Der Menüname ist erforderlich.';
+$string['invalidurl'] = 'Die eingegebene URL ist ungültig und kann nicht verwendet werden. Überprüfen Sie die eingegebene URL.';
+$string['menudisplayerror'] = 'Das zugehörige Flexpage-Menü konnte nicht angezeigt werden. Das Flexpage-Menü ist entweder beschädigt oder nicht mehr vorhanden. Löschen Sie entweder diesen Block oder bearbeiten Sie ihn, damit ein anderes Flexpage-Menü angezeigt wird.';
+$string['dockable'] = 'Fixieren dieses Blocks gestatten';
+$string['dockable_help'] = 'Legt fest, ob dieser Block vom Nutzer/von der Nutzerin fixiert werden kann oder nicht. Beachten Sie, dass die Fixierung durch folgende Ausnahmen deaktiviert werden kann:<ul>
+    <li>Designeinstellungen können das Fixieren von Blöcken verhindern.</li>
+    <li>Nur Flexpage-Menüs, die als Strukturen angezeigt werden, können fixiert werden.</li>
+    <li>Blöcke ohne Titel können nicht fixiert werden.</li>
+</ul>';
+$string['managemenus_help'] = '<p>Menüs ermöglichen Nutzern/Nutzerinnen die erforderliche Navigation durch die Flexpages eines Kurses. Menüs können in beliebigen Flexpage-Bereichen als Blöcke eingefügt werden. Menüs können Links zu Flexpages innerhalb des Kurses, externe URLs und Links zu anderen Menüs enthalten.</p>
+
+<p>Eines der Menüs in einem Kurs kann auch für die Verwendung von &quot;Registerkarten&quot; gekennzeichnet werden. Dieses Menü wird in dem Kurs als Hauptregisterkarten angezeigt. Wenn jede der Registerkarten mehrere Optionen bereitstellen soll, erstellen Sie ein Menü mit Hauptregisterkarten und für jede Registerkarte einen Link zu einem anderen Flexpage-Menü.</p>';
