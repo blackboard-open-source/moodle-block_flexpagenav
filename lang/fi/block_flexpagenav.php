a4c6ca2bd8f6f73809e2e8fae351c1a9542b2f8e
David Castro <david.castro@blackboard.com>
2018-04-23 15:40:30 +0000

    INT-13192: Importing fi lang

---
+++ lang/fi/block_flexpagenav.php
@@ -0,0 +1,108 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Flexpage-valikko';
+$string['flexpagenav:view'] = 'Flexpage-valikkojen tarkastelu';
+$string['flexpagenav:manage'] = 'Flexpage-valikkojen hallinta';
+$string['flexpagenav:addinstance'] = 'Uuden Flexpage-valikkolohkon lisäys';
+$string['addexistingmenuaction'] = 'Lisää nykyinen valikko';
+$string['managemenusaction'] = 'Valikkojen hallinta';
+$string['managemenus'] = 'Valikkojen hallinta';
+$string['name'] = 'Nimi';
+$string['manage'] = 'Hallinta';
+$string['usedastabs'] = 'Käytössä välilehtinä';
+$string['addmenudotdotdot'] = 'Lisää uusi valikko...';
+$string['editmenu'] = 'Muokkaa valikkoa';
+$string['useastab'] = 'Käytä välilehtinä';
+$string['displayname'] = 'Näytä nimi';
+$string['render'] = 'Valikon näyttötapa';
+$string['tree'] = 'Puurakenne';
+$string['dropdown'] = 'Avattava valikko';
+$string['navhorizontal'] = 'Vaakasuuntainen';
+$string['navvertical'] = 'Pystysuuntainen';
+$string['ajaxexception'] = 'Sovellusvirhe: {$a}';
+$string['editmenuaction'] = 'Muokkaa valikkoa';
+$string['deletemenuaction'] = 'Poista';
+$string['managelinksaction'] = 'Linkkien hallinta';
+$string['managelinks'] = 'Linkkien hallinta';
+$string['editlink'] = 'Muokkaa';
+$string['movelink'] = 'Siirrä linkki';
+$string['deletelink'] = 'Poista';
+$string['type'] = 'Tyyppi';
+$string['preview'] = 'Esikatselu/tiedot';
+$string['urllink'] = 'Verkko-osoite';
+$string['addlinkdotdotdot'] = 'Lisää uusi linkki...';
+$string['editingx'] = 'Muokataan {$a}';
+$string['label'] = 'Otsikko';
+$string['url'] = 'Verkko-osoite';
+$string['labelurlrequired'] = 'Otsikko ja Verkko-osoite ovat pakollisia kenttiä.';
+$string['editlinkaction'] = 'Muokkaa';
+$string['movelinkaction'] = 'Siirrä';
+$string['deletelinkaction'] = 'Poista';
+$string['movelinkx'] = 'Siirrä linkki {$a}';
+$string['movebefore'] = 'eteen';
+$string['moveafter'] = 'taakse';
+$string['areyousuredeletelink'] = '<p>Haluatko varmasti poistaa tämän linkin?</p><p>{$a}</p>';
+$string['areyousuredeletemenu'] = 'Haluatko varmasti poistaa valikon <strong>{$a}</strong>?';
+$string['deletemenu'] = 'Poista valikko';
+$string['displaymenu'] = 'Näytä valikko';
+$string['flexpage'] = 'Flexpage-sivu';
+$string['flexpagelink'] = 'Flexpage-sivu';
+$string['includechildren'] = 'Sisällytä alitasot';
+$string['excludechildren'] = 'Sisällytettävät alitasot';
+$string['nochildpages'] = 'Ei flexpage-alisivuja';
+$string['flexpagewithchildren'] = '{$a} ja alitasot';
+$string['flexpagewithoutchildren'] = '{$a} ilman alitasoja';
+$string['flexpageerror'] = 'Virhe: Flexpage-sivua ei enää ole';
+$string['flexpagenav'] = 'Flexpage-valikko';
+$string['flexpagenavlink'] = 'Flexpage-valikko';
+$string['flexpagenavx'] = '{$a}-valikko';
+$string['flexpagenaverror'] = 'Virhe: Flexpage-valikkoa ei enää ole';
+$string['nomenustoadd'] = 'Muita tähän valikkoon lisättäviä Flexpage-valikkoja ei ole.';
+$string['coursemodule'] = 'Kurssiaktiviteetti';
+$string['modlink'] = 'Aktiviteetti';
+$string['ticketlink'] = 'Ongelmatiketti';
+$string['subject'] = 'Aihe';
+$string['labelrequired'] = 'Otsikko on pakollinen kenttä.';
+$string['menus'] = 'Valikot';
+$string['addexistingmenu'] = 'Lisää nykyinen valikko';
+$string['addexistingmenu_help'] = 'Valitse, mihin flexpage-sivun kohtaan haluat lisätä lohkon, valitsemalla jokin <strong>Lisää nykyinen valikko</strong> -ikkunan yläreunassa olevista painikkeista. Napsauta sitten kurssiin lisättävän valikon nimeä.';
+$string['migrationtoptabs'] = 'Kurssin yläosan välilehdet';
+$string['moderror'] = 'Virhe: Aktiviteettia ei enää ole';
+$string['noexistingmenustoadd'] = 'Tälle kurssille ei ole luotu valikoita. Luo uusia valikoita valitsemalla <strong>Hallinta > Valikkojen hallinta</strong>.';
+$string['urlfailedcleaning'] = 'Määritetty verkko-osoite ei läpäissyt tarkistusta ja puhdistusprosessia. Varmista, että annettu osoite on kelvollinen.';
+$string['urlmuststartwith'] = 'Verkko-osoitteen alkuun on lisättävä http:// tai https://';
+$string['managinglinksforx'] = 'Valikon <em>{$a}</em> linkkien hallinta';
+$string['formnamerequired'] = 'Valikon nimi on pakollinen.';
+$string['invalidurl'] = 'Määritetty verkko-osoite on virheellinen eikä sitä voi käyttää. Tarkista osoite.';
+$string['menudisplayerror'] = 'Liitettyä Flexpage-valikkoa ei voitu näyttää. Flexpage-valikko on virheellinen tai sitä ei enää ole. Poista tämä lohko tai muokkaa sitä näyttämään jokin muu Flexpage-valikko.';
+$string['dockable'] = 'Salli lohkon telakointi';
+$string['dockable_help'] = 'Määrittää, voiko käyttäjä telakoida tämän lohkon. Huomaa, että seuraavat rajoitukset voivat estää telakoinnin käyttämisen:
+<ul>
+    <li>Teemassa on lohkojen telakoinnin estävä asetus.</li>
+    <li>Vain puurakenteena näytettävät Flexpage-valikot voidaan telakoida.</li>
+    <li>Lohkoa ei voi telakoida, jos sillä ei ole otsikkoa.</li>
+</ul>';
+$string['managemenus_help'] = '<p>Valikkojen avulla käyttäjät voivat siirtyä kurssin flexpage-sivujen välillä. Valikkoja voi sijoittaa mille tahansa flexpage-sivun alueille lohkoina. Valikot voivat sisältää linkkejä kurssin flexpage-sivuille, ulkoisia verkko-osoitteita tai linkkejä muihin valikkoihin.</p>
+
+<p>Yksi kurssin valikoista voidaan merkitä välilehdiksi. Tämä valikko näytetään kurssin yläosassa välilehtinä. Jos haluat sisällyttää kuhunkin välilehteen useita vaihtoehtoja, luo ylävälilehtien valikko  ja linkitä jokaiseen välilehteen eri Flexpage-valikko.</p>';
