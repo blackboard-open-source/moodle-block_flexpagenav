c31f8e09a02130ca26d129da5f15fff1ca6dc97f
David Castro <david.castro@blackboard.com>
2018-04-23 15:41:05 +0000

    INT-13192: Importing pl lang

---
+++ lang/pl/block_flexpagenav.php
@@ -0,0 +1,108 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Menu Flexpage';
+$string['flexpagenav:view'] = 'Wyświetl menu Flexpage';
+$string['flexpagenav:manage'] = 'Zarządzaj menu Flexpage';
+$string['flexpagenav:addinstance'] = 'Dodaj nowy blok Menu Flexpage';
+$string['addexistingmenuaction'] = 'Dodaj istniejące menu';
+$string['managemenusaction'] = 'Zarządzaj wszystkimi menu';
+$string['managemenus'] = 'Zarządzaj menu';
+$string['name'] = 'Nazwa';
+$string['manage'] = 'Zarządzaj';
+$string['usedastabs'] = 'Użyte jako karty';
+$string['addmenudotdotdot'] = 'Dodaj nowe menu...';
+$string['editmenu'] = 'Edytuj menu';
+$string['useastab'] = 'Użyj jako karty';
+$string['displayname'] = 'Wyświetl nazwę';
+$string['render'] = 'Wyświetl menu jako';
+$string['tree'] = 'Drzewo';
+$string['dropdown'] = 'Menu rozwijane';
+$string['navhorizontal'] = 'Nawigacja pozioma';
+$string['navvertical'] = 'Nawigacja pionowa';
+$string['ajaxexception'] = 'Błąd aplikacji: {$a}';
+$string['editmenuaction'] = 'Edytuj menu';
+$string['deletemenuaction'] = 'Usuń';
+$string['managelinksaction'] = 'Zarządzaj linkami';
+$string['managelinks'] = 'Zarządzaj linkami';
+$string['editlink'] = 'Edytuj';
+$string['movelink'] = 'Przesuń link';
+$string['deletelink'] = 'Usuń';
+$string['type'] = 'Typ';
+$string['preview'] = 'Podgląd/Informacje';
+$string['urllink'] = 'Adres URL';
+$string['addlinkdotdotdot'] = 'Dodaj nowy link...';
+$string['editingx'] = 'Edytowanie {$a}';
+$string['label'] = 'Etykieta';
+$string['url'] = 'Adres URL';
+$string['labelurlrequired'] = 'Pola etykiety i adresu URL są wymagane.';
+$string['editlinkaction'] = 'Edytuj';
+$string['movelinkaction'] = 'Przenieś';
+$string['deletelinkaction'] = 'Usuń';
+$string['movelinkx'] = 'Przenieś link {$a}';
+$string['movebefore'] = 'przed';
+$string['moveafter'] = 'po';
+$string['areyousuredeletelink'] = '<p>Czy na pewno chcesz usunąć ten link?</p><p>{$a}</p>';
+$string['areyousuredeletemenu'] = 'Czy na pewno chcesz usunąć menu <strong>{$a}</strong>?';
+$string['deletemenu'] = 'Usuń menu';
+$string['displaymenu'] = 'Wyświetl menu';
+$string['flexpage'] = 'Flexpage';
+$string['flexpagelink'] = 'Flexpage';
+$string['includechildren'] = 'Dołącz jednostki podrzędne';
+$string['excludechildren'] = 'Jednostki podrzędne do dołączenia';
+$string['nochildpages'] = 'Bez jednostek podrzędnych flexpages';
+$string['flexpagewithchildren'] = '{$a} z jednostkami podrzędnymi';
+$string['flexpagewithoutchildren'] = '{$a} bez jednostek podrzędnych';
+$string['flexpageerror'] = 'Błąd: Flexpage prawdopodobnie już nie istnieje';
+$string['flexpagenav'] = 'Menu Flexpage';
+$string['flexpagenavlink'] = 'Menu Flexpage';
+$string['flexpagenavx'] = 'Menu {$a}';
+$string['flexpagenaverror'] = 'Błąd: Menu Flexpage prawdopodobnie już nie istnieje';
+$string['nomenustoadd'] = 'Do tego menu nie możesz dodać już żadnych innych menu Flexpage.';
+$string['coursemodule'] = 'Aktywność kursu';
+$string['modlink'] = 'Aktywność';
+$string['ticketlink'] = 'Zgłoszenie problemu';
+$string['subject'] = 'Temat';
+$string['labelrequired'] = 'Pole etykiety jest wymagane.';
+$string['menus'] = 'Menu';
+$string['addexistingmenu'] = 'Dodaj istniejące menu';
+$string['addexistingmenu_help'] = 'Wybierz miejsce umieszczenia bloku na stronie Flexpage za pomocą jednego z przycisków u góry okna <strong>Dodaj istniejące menu</strong>. Następnie kliknij nazwę menu, które chcesz dodać do kursu.';
+$string['migrationtoptabs'] = 'Nadrzędne karty kursu';
+$string['moderror'] = 'Błąd: Aktywność prawdopodobnie już nie istnieje';
+$string['noexistingmenustoadd'] = 'Dla tego kursu nie utworzono żadnych menu. Użyj opcji <strong>Zarządzaj > Zarządzaj wszystkimi menu</strong>, aby utworzyć nowe menu.';
+$string['urlfailedcleaning'] = 'Proces walidacji i czyszczenia wprowadzonego adresu URL nie powiódł się. Proszę upewnić się, że wprowadzony adres URL jest poprawny.';
+$string['urlmuststartwith'] = 'Wprowadzony adres URL musi rozpoczynać się od http:// lub https://';
+$string['managinglinksforx'] = 'Zarządzanie linkami dla menu <em>{$a}</em>';
+$string['formnamerequired'] = 'Nazwa menu jest wymagana.';
+$string['invalidurl'] = 'Wprowadzony URL jest niepoprawny i nie może zostać użyty. Proszę zweryfikować wprowadzony URL.';
+$string['menudisplayerror'] = 'Nie udało się wyświetlić powiązanego menu Flexpage. Menu Flexpage jest uszkodzone lub już nie istnieje. Proszę usunąć ten blok lub edytować go, aby wyświetlał inne menu Flexpage.';
+$string['dockable'] = 'Zezwalaj na dokowanie tego bloku.';
+$string['dockable_help'] = 'Określ, czy użytkownik może dokować ten blok. Proszę pamiętać jednak, że dokowanie może być wyłączone w następujących sytuacjach:
+<ul>
+    <li>Ustawienia motywu mogą blokować dokowanie bloków.</li>
+    <li>Można dokować wyłącznie menu Flexpage, które wyświetlane są w formie drzewa.</li>
+    <li>Nie można dokować bloków bez tytułów.</li>
+</ul>';
+$string['managemenus_help'] = '<p>Menu zapewniają nawigację potrzebną użytkownikom do poruszania się po kursie Flexpage. Menu można ustawiać w dowolnych lokalizacjach na stronie jako bloki. Menu mogą zawierać linki do stron Flexpage w ramach kursu, zewnętrznych adresów URL oraz linki do innych menu.</p>
+
+<p>Jedno z menu kursu można oznaczyć również jako korzystające z „kart”. To menu wyświetlone zostanie jako Nadrzędne karty w kursie. Jeśli chcesz, aby każda karta oferowała kilka opcji, utwórz menu Nadrzędne karty i link do innego menu Flexpage dla każdej karty.</p>';
