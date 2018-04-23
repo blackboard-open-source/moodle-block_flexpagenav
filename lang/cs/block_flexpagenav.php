325b1252e372821a27769cc8e3c548554f6b639a
David Castro <david.castro@blackboard.com>
2018-04-23 15:39:16 +0000

    INT-13192: Importing cs lang

---
+++ lang/cs/block_flexpagenav.php
@@ -0,0 +1,108 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Nabídka Flexpage';
+$string['flexpagenav:view'] = 'Zobrazit nabídky Flexpage';
+$string['flexpagenav:manage'] = 'Spravovat nabídky Flexpage';
+$string['flexpagenav:addinstance'] = 'Přidat nový blok Nabídka Flexpage';
+$string['addexistingmenuaction'] = 'Přidat existující nabídku';
+$string['managemenusaction'] = 'Spravovat všechny nabídky';
+$string['managemenus'] = 'Správa nabídek';
+$string['name'] = 'Název';
+$string['manage'] = 'Spravovat';
+$string['usedastabs'] = 'Použito jako karty';
+$string['addmenudotdotdot'] = 'Přidat novou nabídku...';
+$string['editmenu'] = 'Upravit nabídku';
+$string['useastab'] = 'Použít jako karty';
+$string['displayname'] = 'Zobrazovaný název';
+$string['render'] = 'Zobrazit nabídku jako';
+$string['tree'] = 'Strom';
+$string['dropdown'] = 'Rozevírací';
+$string['navhorizontal'] = 'Vodorovná navigace';
+$string['navvertical'] = 'Svislá navigace';
+$string['ajaxexception'] = 'Chyba aplikace: {$a}';
+$string['editmenuaction'] = 'Upravit nabídku';
+$string['deletemenuaction'] = 'Odstranit';
+$string['managelinksaction'] = 'Správa odkazů';
+$string['managelinks'] = 'Správa odkazů';
+$string['editlink'] = 'Upravit';
+$string['movelink'] = 'Přesunout odkaz';
+$string['deletelink'] = 'Odstranit';
+$string['type'] = 'Typ';
+$string['preview'] = 'Náhled/Informace';
+$string['urllink'] = 'Adresa URL';
+$string['addlinkdotdotdot'] = 'Přidat nový odkaz...';
+$string['editingx'] = 'Provádí se úpravy {$a}';
+$string['label'] = 'Popisek';
+$string['url'] = 'Adresa URL';
+$string['labelurlrequired'] = 'Je požadováno pole s popiskem a adresou URL.';
+$string['editlinkaction'] = 'Upravit';
+$string['movelinkaction'] = 'Přesunout';
+$string['deletelinkaction'] = 'Odstranit';
+$string['movelinkx'] = 'Přesunout odkaz {$a}';
+$string['movebefore'] = 'před';
+$string['moveafter'] = 'po';
+$string['areyousuredeletelink'] = '<p>Opravdu chcete odstranit tento odkaz?</p><p>{$a}</p>';
+$string['areyousuredeletemenu'] = 'Opravdu chcete odstranit nabídku <strong>{$a}</strong>?';
+$string['deletemenu'] = 'Odstranit nabídku';
+$string['displaymenu'] = 'Zobrazit nabídku';
+$string['flexpage'] = 'Flexpage';
+$string['flexpagelink'] = 'Flexpage';
+$string['includechildren'] = 'Zahrnout podřazené položky';
+$string['excludechildren'] = 'Podřazené položky k zahrnutí';
+$string['nochildpages'] = 'Žádné podřazené stránky Flexpage';
+$string['flexpagewithchildren'] = '{$a} s podřazenými položkami';
+$string['flexpagewithoutchildren'] = '{$a} bez podřazených položek';
+$string['flexpageerror'] = 'Chyba: Stránka Flexpage již pravděpodobně neexistuje.';
+$string['flexpagenav'] = 'Nabídka Flexpage';
+$string['flexpagenavlink'] = 'Nabídka Flexpage';
+$string['flexpagenavx'] = 'Nabídka {$a}';
+$string['flexpagenaverror'] = 'Chyba: Nabídka Flexpage již pravděpodobně neexistuje.';
+$string['nomenustoadd'] = 'Nejsou k dispozici žádné další nabídky Flexpage, které lze přidat k této nabídce.';
+$string['coursemodule'] = 'Aktivita kurzu';
+$string['modlink'] = 'Aktivita';
+$string['ticketlink'] = 'Záznam o potížích';
+$string['subject'] = 'Předmět';
+$string['labelrequired'] = 'Je požadováno pole s popiskem.';
+$string['menus'] = 'Nabídky';
+$string['addexistingmenu'] = 'Přidat existující nabídku';
+$string['addexistingmenu_help'] = 'Zvolte umístění pro blok na stránce Flexpage tím, že vyberete jedno z tlačítek v horní části okna <strong>Přidat existující nabídku</strong>. Potom klikněte na název nabídky, kterou chcete do kurzu přidat.';
+$string['migrationtoptabs'] = 'Hlavní karty kurzu';
+$string['moderror'] = 'Chyba: Aktivita již pravděpodobně neexistuje.';
+$string['noexistingmenustoadd'] = 'Pro tento kurz nebyly vytvořeny žádné nabídky. K vytvoření nových nabídek použijte nabídku <strong>Spravovat > Spravovat všechny nabídky</strong>.';
+$string['urlfailedcleaning'] = 'Proces ověření a čištění zadané adresy URL se nezdařil. Zkontrolujte, zda je zadaná adresa URL platná.';
+$string['urlmuststartwith'] = 'Zadaná adresa URL musí začínat předponou http:// nebo https://';
+$string['managinglinksforx'] = 'Správa odkazů nabídky <em>{$a}</em>';
+$string['formnamerequired'] = 'Je požadován název nabídky.';
+$string['invalidurl'] = 'Zadaná adresa URL je neplatná a nelze ji použít. Zkontrolujte zadanou adresu URL.';
+$string['menudisplayerror'] = 'Nelze zobrazit související nabídku Flexpage. Nabídka Flexpage je poškozená nebo již neexistuje. Odstraňte tento blok nebo jej upravte tak, aby zobrazoval jinou nabídku Flexpage.';
+$string['dockable'] = 'Povolit ukotvení tohoto bloku';
+$string['dockable_help'] = 'Určuje, zda může být tento blok uživatelem ukotven. Upozorňujeme, že ukotvení lze zakázat pomocí následujících výjimek:
+<ul>
+    <li>Ukotvení bloků může zabránit nastavení motivu.</li>
+    <li>Ukotvit lze pouze nabídky Flexpage zobrazené jako stromové struktury.</li>
+    <li>Bloky bez názvů nelze ukotvit.</li>
+</ul>';
+$string['managemenus_help'] = '<p>Nabídky umožňují navigaci, pomocí které se uživatelé přesouvají mezi stránkami Flexpage kurzu. Nabídky lze umístit do kterýchkoliv oblastí stránek Flexpage jako bloky. Nabídky mohou obsahovat odkazy na stránky Flexpage v rámci kurzu, externí adresy URL a odkazy na další nabídky.</p>
+
+<p>Jednu z nabídek v kurzu lze označit také jako nabídku využívající karty. Tato nabídka se zobrazí v kurzu mezi hlavními kartami. Chcete-li, aby každá z karet nabízela více možností, vytvořte nabídku hlavních karet a odkazy na různé nabídky Flexpage pro každou kartu.</p>';
