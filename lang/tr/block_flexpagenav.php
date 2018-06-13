4023da030cfcdb7a4d7e0ef21394d1ae1220de19
David Castro <david.castro@blackboard.com>
2018-06-13 14:04:46 +0000

    INT-13365: Importing tr lang

---
+++ lang/tr/block_flexpagenav.php
@@ -0,0 +1,108 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Flexpage Menüsü';
+$string['flexpagenav:view'] = 'Flexpage Menülerini Görüntüle';
+$string['flexpagenav:manage'] = 'Flexpage Menülerini Yönet';
+$string['flexpagenav:addinstance'] = 'Yeni bir Flexpage Menü bloku ekle';
+$string['addexistingmenuaction'] = 'Mevcut menü ekle';
+$string['managemenusaction'] = 'Tüm menüleri yönet';
+$string['managemenus'] = 'Menüleri yönet';
+$string['name'] = 'Ad';
+$string['manage'] = 'Yönet';
+$string['usedastabs'] = 'Sekme olarak kullanılır';
+$string['addmenudotdotdot'] = 'Yeni menü ekle...';
+$string['editmenu'] = 'Düzenle menüsü';
+$string['useastab'] = 'Sekme olarak kullan';
+$string['displayname'] = 'Görünen ad';
+$string['render'] = 'Menüyü farklı görüntüle';
+$string['tree'] = 'Ağaç';
+$string['dropdown'] = 'Aşağı açılır';
+$string['navhorizontal'] = 'Yatay gezinti';
+$string['navvertical'] = 'Dikey gezinti';
+$string['ajaxexception'] = 'Uygulama hatası: {$a}';
+$string['editmenuaction'] = 'Düzenle menüsü';
+$string['deletemenuaction'] = 'Sil';
+$string['managelinksaction'] = 'Bağlantıları yönet';
+$string['managelinks'] = 'Bağlantıları yönet';
+$string['editlink'] = 'Düzenle';
+$string['movelink'] = 'Bağlantıyı taşı';
+$string['deletelink'] = 'Sil';
+$string['type'] = 'Tür';
+$string['preview'] = 'Önizleme/Bilgi';
+$string['urllink'] = 'URL';
+$string['addlinkdotdotdot'] = 'Yeni bağlantı ekle...';
+$string['editingx'] = '{$a} düzenleniyor';
+$string['label'] = 'Etiket';
+$string['url'] = 'URL';
+$string['labelurlrequired'] = 'Etiket ve URL alanları gereklidir.';
+$string['editlinkaction'] = 'Düzenle';
+$string['movelinkaction'] = 'Taşı';
+$string['deletelinkaction'] = 'Sil';
+$string['movelinkx'] = '{$a} bağlantısını taşı';
+$string['movebefore'] = 'önce';
+$string['moveafter'] = 'sonra';
+$string['areyousuredeletelink'] = '<p>Bu bağlantıyı silmek istediğinize emin misiniz?</p><p>{$a}</p>';
+$string['areyousuredeletemenu'] = '<strong>{$a}</strong> menüsünü silmek istediğinize emin misiniz?';
+$string['deletemenu'] = 'Menüyü sil';
+$string['displaymenu'] = 'Menüyü görüntüle';
+$string['flexpage'] = 'Flexpage';
+$string['flexpagelink'] = 'Flexpage';
+$string['includechildren'] = 'Alt öğeleri dahil et';
+$string['excludechildren'] = 'Dahil edilecek alt öğeler';
+$string['nochildpages'] = 'Alt flexpage yok';
+$string['flexpagewithchildren'] = 'Alt öğesi olan {$a}';
+$string['flexpagewithoutchildren'] = 'Alt öğesi olmayan {$a}';
+$string['flexpageerror'] = 'Hata: Flexpage muhtemelen artık mevcut değil';
+$string['flexpagenav'] = 'Flexpage Menüsü';
+$string['flexpagenavlink'] = 'Flexpage Menüsü';
+$string['flexpagenavx'] = '{$a} menüsü';
+$string['flexpagenaverror'] = 'Hata: Flexpage menüsü muhtemelen artık mevcut değil';
+$string['nomenustoadd'] = 'Bu menüye ekleyebileceğiniz başka Flexpage Menüsü yok.';
+$string['coursemodule'] = 'Kurs etkinliği';
+$string['modlink'] = 'Etkinlik';
+$string['ticketlink'] = 'Sorun Kaydı';
+$string['subject'] = 'Konu';
+$string['labelrequired'] = 'Etiket alanı gereklidir.';
+$string['menus'] = 'Menüler';
+$string['addexistingmenu'] = 'Mevcut menü ekle';
+$string['addexistingmenu_help'] = 'Bloku Flexpage’in neresine yerleştireceğinizi, <strong>Mevcut menü ekle</strong> penceresinin üst kısmında yer alan düğmelerden birini seçerek belirleyin. Sonra kursa eklemek istediğiniz menünün adını tıklatın.';
+$string['migrationtoptabs'] = 'Kurs Üst Sekmeleri';
+$string['moderror'] = 'Hata: Etkinlik muhtemelen artık mevcut değil';
+$string['noexistingmenustoadd'] = 'Bu kurs için menü oluşturulmadı. Yeni menüler oluşturmak için <strong>Yönet > Tüm menüleri yönet</strong> seçeneğini kullanın.';
+$string['urlfailedcleaning'] = 'Girilen URL için doğrulama ve temizleme işlemi başarısız oldu. Lütfen girilen URL’nin geçerli olduğuna emin olun.';
+$string['urlmuststartwith'] = 'Girilen URL, http:// veya https:// ile başlamalıdır.';
+$string['managinglinksforx'] = '<em>{$a}</em> menüsü için bağlantıların yönetilmesi';
+$string['formnamerequired'] = 'Menü adı gereklidir.';
+$string['invalidurl'] = 'Girilen URL geçersiz ve kullanılamaz. Lütfen girilen URL’yi doğrulayın.';
+$string['menudisplayerror'] = 'İlişkili Flexpage Menüsü görüntülenemiyor. Flexpage Menüsü bozuk veya artık mevcut değil. Lütfen bu bloku silin veya farklı bir Flexpage Menüsü görüntüleyecek şekilde düzenleyin.';
+$string['dockable'] = 'Bu blokun yerleştirilmesine izin ver';
+$string['dockable_help'] = 'Bu blokun kullanıcı tarafından yerleştirilip yerleştirilemeyeceğini belirleyin. Yerleştirme, aşağıdaki istisnalar ile devre dışı bırakılabilir:
+<ul>
+<li>Tema ayarı, blokların yerleştirilmesini önleyebilir.</li>
+<li>Yalnızca Ağaç olarak görüntülenen Flexpage Menüleri yerleştirilebilir.</li>
+<li>Başlıkları olmayan bloklar yerleştirilemez.</li>
+</ul>';
+$string['managemenus_help'] = '<p>Menüler, kullanıcıların bir kursun flexpage’leri arasında gezinmesine olanak tanır. Menüler, herhangi bir flexpage bölgesine blok olarak yerleştirilebilir. Menüler, kurs içindeki flexpage’lere, dış URL’lere veya diğer menü bağlantılarına bağlantılar içerebilir.</p>
+
+<p>Kurstaki menülerden biri, "Sekmeler"i kullanıyor olarak işaretlenebilir. Bu menü, kursta Üst Sekmeler olarak görünecektir. Sekmelerden her birinin birden çok seçenek sunmasını istiyorsanız bir üst menü ve her sekme için bir Flexpage menüsü oluşturun.</p>';
